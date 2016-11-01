50dc6cd7d0c88fcba1683f46c2319f80f510a587
guohongxin <736685433@qq.com>
2016-11-01 10:31:04 +0800

    把代码写的更优雅
    把代码写的更优雅

--- app/app/src/main/java/com/ghx/app/activity/MainActivity.java
+++ app/app/src/main/java/com/ghx/app/activity/MainActivity.java
@@ -1,52 +1,82 @@
 package com.ghx.app.activity;
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import com.ghx.app.R;
+import android.content.ServiceConnection;
+import com.ghx.app.service.MyTestBinder;
 import com.ghx.app.service.MyTestService;
+import android.content.ComponentName;
 import com.ghx.app.utils.Logger;
+import android.content.Intent;
+import android.widget.Button;
+import android.os.IBinder;
+import android.os.Bundle;
+import android.view.View;
+import com.ghx.app.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final java.lang.String TAG_LOG = MainActivity.class.getSimpleName();
+    public static final String TAG_LOG = MainActivity.class.getSimpleName();
 
     private Intent intent;
-    ServiceConnection mConnection;
+    private ServiceConnection mConnection;
+    private Button mBtnStartService;
+    private Button mBtnBindService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         Logger.log_i("MainActivity", "onCreate");
-        intent = new Intent(this, MyTestService.class);
-//        startService(intent);
-        mConnection = new ServiceConnection() {
 
+        initAllView();
 
-            @Override
-            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                Logger.log_i(TAG_LOG, "bindService_onServiceConnected");
-//                ((MyTestBinder)iBinder).binderTest();
-            }
+    }
 
-            @Override
-            public void onServiceDisconnected(ComponentName componentName) {
-                Logger.log_i(TAG_LOG, "bindService_onServiceDisconnected");
-            }
-        };
-        bindService(intent, mConnection, BIND_AUTO_CREATE);
+    private void initAllView() {
+
+        mBtnStartService = (Button) findViewById(R.id.btn_start_service);
+        mBtnBindService = (Button) findViewById(R.id.btn_bind_service);
+
+        mBtnStartService.setOnClickListener(this);
+        mBtnBindService.setOnClickListener(this);
     }
 
     @Override
     protected void onDestroy() {
-//        stopService(intent);
+        stopService(intent);
         unbindService(mConnection);
         super.onDestroy();
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start_service: {
+                intent = new Intent(this, MyTestService.class);
+                startService(intent);
+            }
+            break;
+
+            case R.id.btn_bind_service: {
+                intent = new Intent(this, MyTestService.class);
+                mConnection = new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                        Logger.log_i(TAG_LOG, "bindService_onServiceConnected");
+                        ((MyTestBinder) iBinder).binderTest();
+                    }
+
+                    @Override
+                    public void onServiceDisconnected(ComponentName componentName) {
+                        Logger.log_i(TAG_LOG, "bindService_onServiceDisconnected");
+                    }
+                };
+                bindService(intent, mConnection, BIND_AUTO_CREATE);
+            }
+            break;
+        }
     }
 }
